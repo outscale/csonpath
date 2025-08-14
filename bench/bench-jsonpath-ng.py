09ac9631865cc2f04c8b2199591c8cbc98cfe68f
Matthias Gatto <matthias.gatto@outscale.com>
2025-08-14 14:28:12 +0200

    add python bench
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ bench/bench-jsonpath-ng.py
@@ -0,0 +1,36 @@
+import json
+import time
+from jsonpath_ng import parse
+import csonpath
+
+# Create a large test dataset
+data = {
+    "store": {
+        "book": [{"category": "fiction", "title": f"Book {i}", "price": i % 50 + 5} for i in range(5000)],
+        "bicycle": {"color": "red", "price": 19.95}
+    }
+}
+
+# JSONPath queries to benchmark
+queries = [
+    "$.store.book[*].title",
+    "$..title",
+]
+
+# Benchmark
+for query in queries:
+    start = time.perf_counter()
+    expr = parse(query)
+    for x in range (1000):
+        result = expr.find(data)
+    elapsed = time.perf_counter() - start
+    print(f"jsonpath-ng Query: {query}")
+    print(f"jsonpath-ng Results: {len(result)}, Time: {elapsed:.6f} seconds\n")
+
+    start = time.perf_counter()
+    cp = csonpath.CsonPath(query)
+    for x in range (1000):
+        result = cp.find_all(data)
+    elapsed = time.perf_counter() - start
+    print(f"csonpath python Query: {query}")
+    print(f"csonpath python Results: {len(result)}, Time: {elapsed:.6f} seconds\n")
