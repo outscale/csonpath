9a0d5766c6a82c34b28571d45eaa31edf78385ea
matthias.gatto <matthias.gatto@outscale.com>
2026-04-09 14:26:34 +0200

    fix macos and add benchmark against python
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.com>

--- bench/bench-jsonpath-ng.py
+++ bench/bench-jsonpath-ng.py
@@ -1,5 +1,6 @@
 import json
 import time
+import re
 from jsonpath_ng.ext import parse
 import csonpath
 
@@ -39,3 +40,39 @@ for query in queries:
     elapsed = time.perf_counter() - start
     print(f"csonpath python Query: {query}")
     print(f"csonpath python Results: {len(result)}, Time: {elapsed:.6f} seconds\n")
+
+    start = time.perf_counter()
+    for x in range(1000):
+        if query == "$.store.book[?(@.price) > 20].title":
+            result = []
+            books = data["store"]["book"]
+            for b in books:
+                if b["price"] > 20:
+                    result.append(b["title"])
+        elif query == '$.store.book[?(@.title) =~ "Book"].title':
+            result = []
+            reg = re.compile("Book")
+            books = data["store"]["book"]
+            for b in books:
+                if reg.search(b["title"]):
+                    result.append(b["title"])
+        elif query == "$..title":
+            result = []
+            def rec_get_title(d, out):
+                if isinstance(d, dict):
+                    for dk,dd in d.items():
+                        if dk == "title":
+                            out.append(dd)
+                        rec_get_title(dd, out)
+                elif isinstance(d, list):
+                    for l in d:
+                        rec_get_title(l, out)
+            rec_get_title(data, result)
+        elif query == "$.store.book[*].title":
+            result = []
+            books = data["store"]["book"]
+            for b in books:
+                result.append(b["title"])
+    elapsed = time.perf_counter() - start
+    print(f"pure python Query: {query}")
+    print(f"pure python Results: {len(result)}, Time: {elapsed:.6f} seconds\n")
