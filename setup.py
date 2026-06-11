73a3d1c7242edf420233d87daf47bc2888e664df
matthias.gatto <matthias.gatto@outscale.com>
2026-06-11 19:00:19 +0200

    windows support 3.14 (maybe)
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import sysconfig
 import platform
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
@@ -11,6 +13,33 @@ class BuildExt(build_ext):
         if platform.system() == "Windows" and self.compiler is None:
             self.compiler = "mingw32"
         super().finalize_options()
+
+    def get_libraries(self, ext):
+        libraries = build_ext.get_libraries(self, ext)
+        # distutils/setuptools hardcodes the Python library name as pythonXY
+        # for mingw32, but free-threading builds (3.13+) on Windows ship
+        # pythonXYt.lib instead of pythonXY.lib.
+        # We only "fix" this for 3.14+ because 3.13t is considered
+        # experimental and we prefer not to link against it.
+        if (
+            getattr(self.compiler, "compiler_type", None) == "mingw32"
+            and platform.system() == "Windows"
+            and (sys.version_info.major, sys.version_info.minor) >= (3, 14)
+        ):
+            version_nodot = f"{sys.version_info.major}{sys.version_info.minor}"
+            expected = f"python{version_nodot}"
+            if expected in libraries:
+                for prefix in (sys.base_prefix, sysconfig.get_config_var("prefix")):
+                    if not prefix:
+                        continue
+                    libs_dir = os.path.join(prefix, "libs")
+                    if os.path.exists(os.path.join(libs_dir, f"{expected}t.lib")):
+                        libraries = [
+                            f"{expected}t" if lib == expected else lib
+                            for lib in libraries
+                        ]
+                        break
+        return libraries
 
 
 # Base sources
